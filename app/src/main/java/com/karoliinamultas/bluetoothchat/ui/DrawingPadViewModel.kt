704b4e9116502905d4c0c8dd9da02f7f7b8f853c
vilimaki <vilimaki@metropolia.fi>
2023-03-12 22:58:44 +0200

    refractored myViewModel

--- app/src/main/java/com/karoliinamultas/bluetoothchat/ui/DrawingPadViewModel.kt
+++ app/src/main/java/com/karoliinamultas/bluetoothchat/ui/DrawingPadViewModel.kt
@@ -1,13 +1,8 @@
 package com.karoliinamultas.bluetoothchat.ui
 
 import android.app.Application
-import android.content.Context
 import android.graphics.Bitmap
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
 import com.karoliinamultas.bluetoothchat.BluetoothChatApplication
 import id.zelory.compressor.Compressor
 import id.zelory.compressor.constraint.format
@@ -20,64 +15,9 @@ import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.nio.file.Files
-import java.util.zip.Deflater
-import java.util.zip.Inflater
 
 
 class DrawingPadViewModel(application: Application): AndroidViewModel(application) {
-
-    fun compressByteArray(byteArray: ByteArray): ByteArray {
-        val input = byteArray
-        val output = ByteArray(input.size*4)
-        val compressor = Deflater().apply {
-            setInput(input)
-            finish()
-        }
-        val compressedDataLength = compressor.deflate(output)
-        Log.d("DBG", "compress $compressedDataLength")
-        Log.d("DBG", "output copy ${output.copyOfRange(0, compressedDataLength).size}")
-        return output.copyOfRange(0, compressedDataLength)
-    }
-
-    fun decompressByteArray(byteArray: ByteArray): ByteArray {
-        val input = byteArray
-        val output = ByteArray(input.size*4)
-        val inflater = Inflater().apply {
-            setInput(input)
-        }
-        val decompressedDataLength = inflater.inflate(output)
-        Log.d("DBG", "decompress $decompressedDataLength")
-        Log.d("DBG", "output copy ${output.copyOfRange(0, decompressedDataLength).size}")
-        return output.copyOfRange(0, decompressedDataLength)
-    }
-
-//    fun decompressByteArray(byteArray: ByteArray): ByteArray {
-//        val inflater = Inflater()
-//        val outputStream = ByteArrayOutputStream()
-//
-//        return outputStream.use {
-//            val buffer = ByteArray(1024)
-//
-//            inflater.setInput(byteArray)
-//            var count = -1
-//            while (count != 0) {
-//                count = inflater.inflate(buffer)
-//                outputStream.write(buffer, 0, count)
-//            }
-//            inflater.end()
-//            outputStream.toByteArray()
-//        }
-//    }
-
-//    fun bitmapToByteArray(bitmap: Bitmap, format: Bitmap.CompressFormat = Bitmap.CompressFormat.JPEG, quality: Int = 90): ByteArray {
-//        val outputStream = ByteArrayOutputStream()
-//        bitmap.compress(format, quality, outputStream)
-//        val byteArray = outputStream.toByteArray()
-//        compressByteArray(byteArray)
-//        Log.d("DBG", "${byteArray.size}")
-//        return byteArray
-//    }
-//    fun decompressByteArray
 
     val context = getApplication<BluetoothChatApplication>()
     suspend fun bitmapToByteArray(bitmap: Bitmap): ByteArray {
@@ -109,18 +49,3 @@ class DrawingPadViewModel(application: Application): AndroidViewModel(applicatio
         }
     }
 }
-
-
-
-//
-//        val outputStream = ByteArrayOutputStream()
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-//        var quality = 90
-//        while (outputStream.toByteArray().size / 1024 > 1 && quality > 0) {
-//            outputStream.reset()
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-//            quality -= 10
-//        }
-//        return outputStream.toByteArray()
-//    }
-
